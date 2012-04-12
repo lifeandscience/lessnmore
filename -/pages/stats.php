927e168d1af5e79d156cd162b199fd8f2341db11
Ben Schell <ben@benschell.org>
2012-04-12 10:20:32 -0400

    Adding "Mark Gone" capabilities.

--- -/pages/stats.php
+++ -/pages/stats.php
@@ -7,6 +7,8 @@
 		<th>URL</th>
 		<th>Lessn'd</th>
 		<th>Hits</th>
+		<th>Active?</th>
+		<th>Mark Gone</th>
 	</tr>
 	
 <?php foreach($top_urls as $url) { 
@@ -16,6 +18,8 @@
 		<td><?php echo htmlspecialchars($url['url'], ENT_QUOTES, 'UTF-8')?></td>
 		<td><!-- <a href="<?php /*=$short*/ ?>"> --><?php echo $short?><!-- </a> --></td>
 		<td><?php echo $url['hits']?></td>
+		<td><?php echo $url['redir_type'] == 'gone' ? 'Gone' : 'Active'; ?></td>
+		<td><?php if($url['redir_type'] != 'gone'){ ?><a href="?mark_gone=1&slug=<?php echo $url['custom_url']; ?>">Mark Gone</a><?php } ?></td>
 	</tr>
 <?php } ?>
 
